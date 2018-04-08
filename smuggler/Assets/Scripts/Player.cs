d492d4b681afd3b6409ae741ba87cd116cf7444f
Adam Duvin (RIT Student) <axd3010@ad.rit.edu>
2018-04-08 16:31:13 -0400

    Set up scene

---
+++ smuggler/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private BoxCollider2D boundingBox;
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        boundingBox = gameObject.GetComponent<BoxCollider2D>();
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Move()
+    {
+
+    }
+}
