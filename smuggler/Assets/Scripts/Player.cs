80885ef2dfb3a05e592238e876719bf59e7fa60c
adamduvin <axd3010@g.rit.edu>
2018-04-11 11:44:45 -0400

    Worked on movement

--- smuggler/Assets/Scripts/Player.cs
+++ smuggler/Assets/Scripts/Player.cs
@@ -7,18 +7,27 @@ public class Player : MonoBehaviour {
     private Rigidbody2D rb;
     private BoxCollider2D boundingBox;
     private Camera mainCamera;
+    private Vector3 position;
+    private float steering; 
 
 	// Use this for initialization
 	void Start () {
         rb = gameObject.GetComponent<Rigidbody2D>();
         boundingBox = gameObject.GetComponent<BoxCollider2D>();
         mainCamera = Camera.main;
+        position = transform.position;
+        steering = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void FixedUpdate()
+    {
+
+    }
 
     void Move()
     {
