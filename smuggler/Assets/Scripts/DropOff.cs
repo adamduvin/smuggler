219e5e721073bccabffc4b3bac9c06a52a243151
Timothy Carter <txc1321@rit.edu>
2018-04-18 20:34:34 -0400

    Added Scripts for obstacles/objectives
    Need collision logic to complete, objects not showing up in game

---
+++ smuggler/Assets/Scripts/DropOff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropOff : MonoBehaviour {
+
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if(Collision) collision logic
+		if(player.hasSupplies)
+		{
+			//Application.LoadLevel("gameOver");
+			Debug.Log("1");
+		}
+	}
+}
